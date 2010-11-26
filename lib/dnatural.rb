fd5951aa13bae9b043b0387e1c16b77aa313cfa8
Chris Beer <chris@cbeer.info>
2010-11-26 18:24:11 -0500

    add manifest updates to add/rm

--- lib/dnatural.rb
+++ lib/dnatural.rb
@@ -19,17 +19,50 @@ module Dnatural
       d
     end
 
+    def manifest
+      @manifest ||= Checkm::Manifest.new open(File.join(path, 'manifest.txt')).read 
+    end
+
+    def manifest!
+      @manifest = nil
+      manifest
+    end
+
     def list
       glob('**/*')
     end
 
     def add src, dest, options = {}
       FileUtils.cp src, File.join(path, dest), options
+
+      manifest!
+
+      lock
+      m = manifest.add dest, :base => path
+      File.open(File.join(path, 'manifest.txt'), 'w') do |f|
+        f.write(m.to_s)
+      end
+
+      unlock
     end
 
     def remove list, options = {}
       list = [list] if list.instance_of? String 
       FileUtils.rm list.map { |x| File.join(path, x) }, options
+
+      m = manifest!
+      lock
+
+      list.each do |l|
+        m = m.remove l, :base => path
+      end
+
+      File.open(File.join(path, 'manifest.txt'), 'w') do |f|
+        f.write(m.to_s)
+      end
+
+      unlock
     end
+
   end
 end
